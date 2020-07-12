05990469c1337e670daef2c0a42cfb46e48a726c
binamrathapa <binamrathapa@users.noreply.github.com>
2020-07-12 13:31:40 +0545

    Add files via upload

---
+++ Web_MVC/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_MVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Address { get; set; }
+    }
+}
